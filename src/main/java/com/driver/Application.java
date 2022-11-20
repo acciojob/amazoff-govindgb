e90e2b96d1e6dc07665b97f8faf42aafaac7a6f2
govindgb <govindbhattpnd@gmail.com>
2022-11-20 13:27:34 +0530

    third

--- src/main/java/com/driver/Application.java
+++ src/main/java/com/driver/Application.java
@@ -1,8 +1,8 @@
 package com.driver;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+		import org.springframework.boot.SpringApplication;
+		import org.springframework.boot.autoconfigure.SpringBootApplication;
+		import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
